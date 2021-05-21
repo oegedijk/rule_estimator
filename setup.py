b549e616b836c4c3588f24524c713833f42d155c
Oege Dijk <oegedijk@gmail.com>
2021-05-21 13:10:51 +0200

    fix setup.py packages

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='rule_estimator',
     version='0.0.1',
+    packages=['rule_estimator'],
     description='Scikit-learn compatible business rule estimator',
     long_description='Scikit-learn compatible business rule estimator',
-    license='MIT',
+    license='Apache-2',
     classifiers=["Development Status :: 3 - Alpha"],
     install_requires=['pandas', 'numpy', 'scikit-learn', 'oyaml'],
     python_requires='>=3.6',
