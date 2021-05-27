8d3530012e8f94328d31961aad72df41b47dae77
Oege Dijk <oegedijk@gmail.com>
2021-05-27 09:54:54 +0200

    changed igraph dependency name

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     long_description='Scikit-learn compatible business rule estimator',
     license='Apache-2',
     classifiers=["Development Status :: 3 - Alpha"],
-    install_requires=['pandas', 'numpy', 'scikit-learn', 'oyaml', 'igraph'],
+    install_requires=['pandas', 'numpy', 'scikit-learn', 'oyaml', 'python-igraph'],
     python_requires='>=3.6',
     author='Oege Dijk',
     author_email='oegedijk@gmail.com',
