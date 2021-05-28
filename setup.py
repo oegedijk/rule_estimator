4a3b0d97a15cdb2c5a4fc5f80083a1d673eb6492
Oege Dijk <oegedijk@gmail.com>
2021-05-28 12:39:33 +0200

    bumps version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rule_estimator',
-    version='0.2.0',
+    version='0.2.1',
     packages=find_packages(),
     description='Scikit-learn compatible business rule estimator',
     long_description='Scikit-learn compatible business rule estimator',
